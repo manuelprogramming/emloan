12654e44a4aa98aa763b709b21fdcf8129087ad4
Manuel <manuel.pegler@gmail.com>
2026-02-06 17:44:47 +0100

    removed file loading for start_immo

--- emloan/start_immo.py
+++ emloan/start_immo.py
@@ -1,20 +1,29 @@
-from pathlib import Path
-import json
-
 from . import calculators
 from . import immo
 
 
+prop_data = {
+    "details": {"living_space": 100},
+    "base_cost": {
+        "price": 375000,
+        "notary_rate": 0.015,
+        "property_buy_tax_rate": 0.05,
+        "land_registry_rate": 0.005,
+        "agent_rate": 0.0357,
+        "proprietary_capital_rate": 0.2,
+        "loan_rate": 0.8,
+    },
+    "cash_flow": {
+        "period": "monthly",
+        "net_cold_rent": 1030,
+        "operating_expanses": 250,
+        "operating_income": 200,
+    },
+}
+
+
 def start_immo() -> immo.Immo:
-    repay_rate = 0.08
-    annuity = 1400 * 12
     interest_rate = 0.0325
     period = 25
-
-    data_dir = Path("data")
-    cur_file = data_dir / "house.json"
-
-    with open(cur_file) as json_file:
-        prop_data = json.load(json_file)
 
     return calculators.calc_property_by_period(prop_data, interest_rate, period)
